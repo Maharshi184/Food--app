d2f6a14ef5c74eb2745961dc8d4ff194dfb107ec
Maharshi184 <140582672+Maharshi184@users.noreply.github.com>
2024-12-24 10:18:49 +0530

    Update mogodb.dart

--- lib/dbHelper/mogodb.dart
+++ lib/dbHelper/mogodb.dart
@@ -12,10 +12,6 @@ class mongodb {
       ordersCollection,
       ownerCollection;
 
-  // db = Db.create(MONGO_CONNECTION_URI);
-  // db.open();
-  // userCollection = db.collection(USER_COLLECTION);
-
   static connect() async {
     // Replace the connection string with your MongoDB URI
     db = await Db.create(MONGO_CONNECTION_URI);
@@ -83,52 +79,5 @@ class mongodb {
 
 
 
-// static var db, userCollection;
 
-  // static connect() async {
-  //   // Replace the connection string with your MongoDB URI
-  //   db = await Db.create("mongodb+srv://maharshi:200405@cluster0.mongodb.net/FoodApp_108?retryWrites=true&w=majority");
-
-  //   await db.open();
-
-  //   // Replace 'users' with your collection name
-  //   userCollection = db.collection('Login_credentials');
-  // }
-
-  // static Future<List<Map<String, dynamic>>> getData() async {
-  //   return await userCollection.find().toList();
-  // }
-
-  // static updateData(Map<String, dynamic> query, Map<String, dynamic> updateData) async {
-  //   await userCollection.update(query, updateData);
-  // }
-
-  // static deleteData(Map<String, dynamic> query) async {
-  //   await userCollection.remove(query);
-  // }
-
-  // static closeConnection() async {
-  //   await db.close();
-  // }
-
-
-
-
-
-// import 'package:meal/dbHelper/Constant.dart';
-// import 'package:mongo_dart/mongo_dart.dart';
-
-// const MONGO_CONNECTION_URI = "mongodb+srv://maharshi:200405@cluster0.9rfuw.mongodb.net/";
-// const USER_COLLECTION="Login_data";
-
-// class mongodb
-// {
-//   static var db, USER_COLLECTION;
-//   static connect() async{
-//     db = await  Db.create(MONGO_CONNECTION_URI);
-//     await db.open();
-//     USER_COLLECTION = db.collection();
-    
-//   }
-// }
-
+  
